c2ab5c533a0eeb5a15ba5c1087df3de0924b9d00
sadia mahamoud <sadimoa06@gmail.com>
2023-11-05 22:43:51 -0800

    changed

--- api/Lessons/lesson.js
+++ api/Lessons/lesson.js
@@ -5,14 +5,16 @@ import permission from "../middleware/permission.js";
 
 const router = express.Router();
 
+// get all the lesson
 router.get("/", permission, async (req, res) => {
   try {
-    const lesson = await prisma.lesson.findMany();
+    const lessons = await prisma.lesson.findMany();
 
-    if (lesson) {
-      res.status(201).json(lesson);
+    if (lessons) {
+      res.status(200).json(lessons);
     } else {
-      res.status(404).json({ message: "User not found" });
+      // Respond with a 404 Not Found status if no lessons are found
+      res.status(404).json({ message: "Lessons not found" });
     }
   } catch (err) {
     res.status(500).json({
@@ -22,14 +24,36 @@ router.get("/", permission, async (req, res) => {
   }
 });
 
+// get lesson by id
+router.get("/:id", permission, async (req, res) => {
+  try {
+    const lesson = await prisma.lesson.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (lesson) {
+      res.status(200).json(lesson);
+    } else {
+      // Respond with a 404 Not Found status if no lessons are found
+      res.status(404).json({ message: "Lesson not found" });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "something went wrong",
+      err: err.message,
+    });
+  }
+});
 
 // Create a lesson
-router.post("/create", permission, async (req, res) => {
-  const { title, videoUrl, content, isPublished, userId, categoryId} = req.body;
+router.post("/add", permission, async (req, res) => {
+  const { title, videoUrl, content, isPublished } = req.body;
 
   try {
-     // Check if a lesson with the same characteristics already exists
-     const existingLesson = await prisma.lesson.findFirst({
+    // Check if a lesson with the same values already exists
+    const existingLesson = await prisma.lesson.findFirst({
       where: {
         title: title,
         videoUrl: videoUrl,
@@ -37,24 +61,20 @@ router.post("/create", permission, async (req, res) => {
       },
     });
 
+    // if the lesson already exist return 404 error
     if (existingLesson) {
+      // Respond with a 409  status if lessons already exizt
       return res.status(409).json({ message: "Lesson already exists" });
     }
 
-    // Create the lesson
+    // Create new lesson
     const lesson = await prisma.lesson.create({
-      data: {
-        title: title,
-        videoUrl: videoUrl,
-        content: content,
-        isPublished: isPublished,
-        // Associate the lesson with the specified user
-      },
+      data: req.body,
     });
 
-    // Return the lesson you've created
+    // response 201 a created stats and the lesson you've created
     res.status(201).json({
-      message: "Lesson created successfully",
+      message: "Lesson created succesfully",
       lesson: lesson,
     });
   } catch (err) {
@@ -65,5 +85,61 @@ router.post("/create", permission, async (req, res) => {
   }
 });
 
+// Update the lesson
+router.put("/update/:id", permission, async (req, res) => {
+  try {
+    // find the the lesson you wanna update by id
+    const lesson = await prisma.lesson.update({
+      where: {
+        id: req.params.id,
+      },
+      data: req.body,
+    });
 
+    // response 201 a created stats and the lesson you've created
+    if (lesson) {
+      res.status(201).json(lesson);
+    } else {
+      // Respond with a 404 Not Found status if the lesson is not found
+      return res.status(409).json({ message: "Lesson already exists" });
+    }
+  } 
+  catch (err) {
+    // Handle any unexpected errors with a 500 Internal Server Error status
+    res.status(500).json({
+      message: "Error creating lesson",
+      err: err.message,
+    });
+  }
+});
+
+
+// delete the lesson
+router.delete("/delete/:id", permission, async (req, res) => {
+  try {
+    // find the the lesson you wanna delete
+    const lesson = await prisma.lesson.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    // check if the lesson exist
+    if (lesson) {
+      res.status(204).json({message: "lesson deleted succesfully"});
+    } else {
+      return res.status(404).json({ message: "Lesson not found" });
+    }
+  } catch (err) {
+    // Handle any unexpected errors with a 500 Internal Server Error status
+    res.status(500).json({
+      message: "Error creating lesson",
+      err: err.message,
+    });
+  }
+});
+
+
+
+// export the lesson router
 export default router;
--- api/Lessons/lossenCategory.js
+++ api/Lessons/lossenCategory.js
@@ -5,14 +5,37 @@ import permission from "../middleware/permission.js";
 
 const router = express.Router();
 
+// get all the Category
 router.get("/", permission, async (req, res) => {
   try {
     const category = await prisma.category.findMany();
 
     if (category) {
-     return res.status(200).json(category);
+      return res.status(200).json(category);
     } else {
-     return res.status(404).json({ message: "category not found" });
+      return res.status(404).json({ message: "category not found" });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "something went wrong",
+      err: err.message,
+    });
+  }
+});
+
+// get the Category by id
+router.get("/:id", permission, async (req, res) => {
+  try {
+    const category = await prisma.category.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+
+    if (category) {
+      return res.status(200).json(category);
+    } else {
+      return res.status(404).json({ message: "category not found" });
     }
   } catch (err) {
     res.status(500).json({
@@ -24,41 +47,86 @@ router.get("/", permission, async (req, res) => {
 
 // Add category
 router.post("/add", permission, async (req, res) => {
-  const { name } = req.body;
-
   try {
     // find the category
     const existingCategory = await prisma.category.findUnique({
       where: {
-        name: name,
+        name: req.body.name,
       },
     });
 
-    // look if the category is already exist
+    // check if the category is already exist
     if (existingCategory) {
-      // create the category
       return res.status(409).json({ message: "category already exists" });
     }
 
+    // create the category
     const category = await prisma.category.create({
-      data: {
-        name: name,
-      },
+      data: req.body,
     });
-    
 
     // return the category you've created
     return res.status(201).json({
       message: "category created succesfully",
       category: category,
     });
-
   } catch (err) {
     res.status(500).json({
       message: "Error creating category",
       err: err.message,
     });
   }
+});
+
+// Edit Category
+router.put("/update/:id", async (req, res) => {
+  // check if the ID you provide is exist, then update
+  try {
+    const category = await prisma.category.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: req.body,
+    });
+
+    // return the category you'v updated
+    if (category) {
+      res.status(201).json(category);
+    } else {
+      return res.status(404).json({ message: "category not found" });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error updating category",
+      err: err.message,
+    });
+  }
+});
+
+// delete category
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const category = await prisma.category.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    if (!category) {
+      res.status(404).json({
+        message: "Category not found",
+      });
+    } else {
+      res.status(204).json({
+        message: "Category deleted successfully",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error deleting category",
+      err: err.message,
+    });
+  }
+  
 });
 
 export default router;
